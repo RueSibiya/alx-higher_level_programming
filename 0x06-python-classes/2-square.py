739f27258a6eda533ebf2d99559c90251fa49dac
RueSibiya <rksibiya1@gmail.com>
2023-10-29 02:17:19 -0700

    1-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError
+        elif size < 0:
+            raise ValueError
+	self.__size = size
