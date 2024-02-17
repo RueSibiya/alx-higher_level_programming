b22dc673333046a9ca75c04b52f9df4edf8d71c6
RueSibiya <rksibiya1@gmail.com>
2024-02-17 06:13:27 -0800

    10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Lists all records of the database hbtn_0c_0
 -- Score are ordered in descending order
-SELECT 'score','name' 
-FROM 'second_table'
-ORDER BY 'score' DESC;
+SELECT `score`,`name` 
+FROM `second_table`
+ORDER BY `score` DESC;
