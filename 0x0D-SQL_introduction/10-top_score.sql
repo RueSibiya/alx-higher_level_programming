73793b38cb791136a1069ea0d891934be002ef16
RueSibiya <rksibiya1@gmail.com>
2024-02-17 05:56:47 -0800

    sql

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- Lists all records of the database hbtn_0c_0
+-- Score are ordered in descending order
+SELECT 'score','name' 
+FROM 'second_table'
+ORDER BY 'score' DESC;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Deletes the database hbtn_0c_0 from my MySQL server.
+-- If it doesnt exist the script should not fail.
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
