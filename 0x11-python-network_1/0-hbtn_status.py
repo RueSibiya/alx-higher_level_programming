c23595c3e04c8563a74b93681af1da4d9831d408
RueSibiya <rksibiya1@gmail.com>
2024-04-07 22:50:28 -0700

    updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-This script fetches https://intranet.hbtn.io/status
-"""
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
 from urllib.request import Request, urlopen
 
+
 if __name__ == "__main__":
-    req = Request("https://intranet.hbtn.io/statu")
+    req = Request("https://alx-intranet.hbtn.io/status")
     with urlopen(req) as response:
-        the_page = response.read()
+        body = response.read()
         print("Body response:")
-        print("\t- type: {}".format(type(the_page)))
-        print("\t- content: {}".format(the_page))
-        print("\t- utf8 content: {}".format(the_page.decode("utf-8")))
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
